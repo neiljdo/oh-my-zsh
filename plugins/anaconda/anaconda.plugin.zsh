75675554bbd5b6a1c844a23539b6c2cf0c4f904f
Neil John D. Ortega <neiljohn.ortega@gmail.com>
2014-05-18 22:18:01 +0800

    Update conda source activate alias to also modify $PYTHONPATH

--- plugins/anaconda/anaconda.plugin.zsh
+++ plugins/anaconda/anaconda.plugin.zsh
@@ -1,14 +1,20 @@
 # Append Anaconda install location to path
 export PATH=$HOME/$ANACONDA/bin:$PATH
 
-# Append additional paths
+declare -U pythonpath
+
+# Append additional paths to PYTHONPATH
 # From http://earthwithsun.com/questions/598810/zsh-config-to-export-or-not-to-export
-typeset -U path
-path=($ANACONDA_ADDITIONAL_PATHS $path)
-export path
+function update_pythonpath() {
+    pythonpath=($ANACONDA_ADDITIONAL_PATHS $pythonpath)
+}
+
+function cnsa() {
+    source activate "$1" && update_pythonpath
+}
 
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 
 # Aliases
-alias cn.sa='source activate'
+alias cn.sa='cnsa'
 alias cn.sd='source deactivate'
