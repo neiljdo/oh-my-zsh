71855304c608a058b5963a2436abd61415facf20
Neil John D. Ortega <neiljohn.ortega@gmail.com>
2014-05-18 20:09:29 +0800

    Updates to anaconda plugin

--- plugins/anaconda/anaconda.plugin.zsh
+++ plugins/anaconda/anaconda.plugin.zsh
@@ -1,8 +1,14 @@
 # Append Anaconda install location to path
 export PATH=$HOME/$ANACONDA/bin:$PATH
 
+# Append additional paths
+# From http://earthwithsun.com/questions/598810/zsh-config-to-export-or-not-to-export
+typeset -U path
+path=($ANACONDA_ADDITIONAL_PATHS $path)
+export path
+
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
 
-# aliases
+# Aliases
 alias cn.sa='source activate'
 alias cn.sd='source deactivate'
